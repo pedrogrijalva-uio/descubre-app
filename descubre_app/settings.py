22d4dd489213ba1f525e89f18b8cf982b5d54fec
Pedro Marcelo Grijalva Grijalva <pgrijalva@mag.gob.ec>
2021-01-12 21:47:23 -0500

    Added database config

--- descubre_app/settings.py
+++ descubre_app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,10 @@ WSGI_APPLICATION = 'descubre_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'descubre',
+        'USER': 'postgres',
+        'PASSWORD': 'admin123.'
     }
 }
 
